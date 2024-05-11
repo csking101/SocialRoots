afc742b9d5b1ceea0dc26ec4c03ea31042cd71a8
csking101 <chinmayasahu101@gmail.com>
2024-05-12 00:54:55 +0530

    Obtained sample results

---
+++ server/ml/sample_results.py
@@ -0,0 +1,5 @@
+ipm_results = ['Project 17', 'Project 5', 'Project 19', 'Project 11', 'Project 4', 'Project 20', 'Project 16', 'Project 10', 'Project 3', 'Project 15', 'Project 1', 'Project 13', 'Project 2', 'Project 12', 'Project 18', 'Project 6', 'Project 14', 'Project 7', 'Project 9', 'Project 8']
+
+ppm_results = ['Project 4', 'Project 2', 'Project 20', 'Project 6', 'Project 1', 'Project 17', 'Project 18', 'Project 10', 'Project 15', 'Project 13', 'Project 16', 'Project 14', 'Project 5', 'Project 12', 'Project 3', 'Project 19', 'Project 9', 'Project 7', 'Project 8', 'Project 11']
+
+iim_results = ['Investor 9', 'Investor 4', 'Investor 1', 'Investor 10', 'Investor 5', 'Investor 2', 'Investor 7', 'Investor 3', 'Investor 8', 'Investor 6']
