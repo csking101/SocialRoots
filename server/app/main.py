40662d5fbd16468a8b42015c419f373a2d692827
marcdhi <gandhi.mardav@gmail.com>
2024-05-12 00:33:31 +0530

    server: add get routes for supabase data

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
+import requests
+
+from helpers.json_to_csv import json_to_csv
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/getProjects")
+async def getProjects():
+    response = requests.get("http://localhost:3000/api/getProjects")
+    json_to_csv(response.json(), "projects")
+    return response.json()
+
+
+@app.get("/getInvestors")
+async def getProjects():
+    response = requests.get("http://localhost:3000/api/getInvestors")
+    json_to_csv(response.json(), "investors")
+    return response.json()
+
+
+
+
 
