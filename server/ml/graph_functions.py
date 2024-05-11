adfc3fe82fafa695dc45dba7cba1a7dac4942f5c
csking101 <chinmayasahu101@gmail.com>
2024-05-11 16:40:07 +0530

    Made prompts for similarity

--- server/ml/graph_functions.py
+++ server/ml/graph_functions.py
@@ -14,6 +14,7 @@ The mapping for similarity is as follows(Investor to Project):
 6) Region -> Region (cardinal index)
 7) Background/Experience -> Resources Required (LLM for applicability)
 """
+from .prompting import area_of_expertise_match_prompt, risk_appetite_match_prompt, background_match_prompt
 
 def investment_scale_match(investment_scale, current_status):
     diff = abs(investment_scale - current_status)
@@ -21,6 +22,28 @@ def investment_scale_match(investment_scale, current_status):
     match_factor = 1/(1 + diff)
     
     return match_factor
+
+def area_of_interest_match(investor_aoi, project_aoi):
+    return int(investor_aoi == project_aoi)
+
+def area_of_expertise_match(area_of_expertise, resources_required):
+    return area_of_expertise_match_prompt(area_of_expertise, resources_required)
+
+def risk_appetite_match(risk_appetite, description):
+    return risk_appetite_match_prompt(risk_appetite, description)
+
+def delivery_time_match(delivery_time, project_delivery_time):
+    diff = abs(delivery_time - project_delivery_time)
+    
+    match_factor = 1/(1 + diff)
+    
+    return match_factor
+
+def region_match(investor_region, project_region):
+    return int(investor_region == project_region)
+
+def background_match(investor_background, resources_required):
+    return background_match_prompt(investor_background, resources_required)
 
 def graph_train(graph, num_epochs):
     """
